0dc543008330e61704be247dcb5a7025c5024648
Dari Alves <darieltondf@gmail.com>
2022-10-11 20:40:50 -0300

    built-in types

--- dart_and_flutter/001/dart_01.dart
+++ dart_and_flutter/001/dart_01.dart
@@ -15,9 +15,43 @@ void variables() {
   print(name_02.runtimeType);
 
   print(name_03.runtimeType);
+
+  print('\n');
+}
+
+void printAList(List<int> number) {
+  print(number);
 }
 
 void types() {
-  int? x;
-  print(x);
+  List<int> list = [];
+  list.add(1);
+  list.add(2);
+
+  var list_2 = <int>[];
+  list_2.add(3);
+  list_2.add(4);
+
+  dynamic list_3 = <int>[];
+  list_3.add(5);
+  list_3.add(6);
+
+  printAList(list);
+  printAList(list_2);
+  printAList(list_3);
 }
+
+/*
+* Other built-in types
+*
+* Numbers (int, double)
+* Strings (String)
+* Boolean (bool)
+* List (List)
+* Sets (Set)
+* Maps (Map)
+* Runes (Runes)
+* Symbols (Symbol)
+* The value null (Null)
+*
+* */
